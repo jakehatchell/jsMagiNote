5ee6470c24b520c57a57e6bb839f6b773bdfd995
Jake Hatchell <jwhatch@clemson.edu>
2026-02-16 16:09:49 -0500

    Add delete and patch.

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -25,6 +25,31 @@ exports.createNote = async (req, res) => {
     }
 };
 
+exports.updateNote = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { text } = req.body;
+
+        const [rows] = await pool.query(
+            "select formatted_md from notes where noteID = ?",
+            [id]
+        );
+
+        if (!rows)
+
+        rows = await pool.query(
+            "update notes set raw_text = ? where noteID = ?",
+            [text, id]
+        );
+
+
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Database error." });
+    }
+};
+
 exports.getNote = async (req, res) => {
     try {
         const { id } = req.params;
@@ -39,6 +64,20 @@ exports.getNote = async (req, res) => {
         }
 
         res.json(rows[0]);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Database error." });
+    }
+};
+
+exports.getAllNotes = async (req, res) => {
+    try {
+        const [rows] = await pool.query(
+            "select * from notes"
+        );
+
+        res.json(rows);
     }
     catch (err) {
         console.error(err);
@@ -71,4 +110,18 @@ exports.formatNote = async (req, res) => {
     console.error(err);
     res.status(500).json({ error: "Formatting failed" });
   }
+};
+
+exports.deleteNote = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await pool.query(
+            "DELETE FROM notes WHERE noteID = ?",
+            [id]
+        );
+        res.json({ id: Number(id) });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Delete failed" });
+    }
 };
--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -1,12 +1,20 @@
 const router = require("express").Router();
 
-const { createNote, getNote, formatNote } = require("../controllers/noteController");
+const { createNote, getNote, getAllNotes, formatNote, updateNote, deleteNote } = require("../controllers/noteController");
 
 // Make this /api/formatNotes accessible.
+// Get note by ID
 router.get("/notes/:id", getNote);
+// Get all notes
+router.get("/notes", getAllNotes);
+// Create a new note
 router.post("/notes", createNote)
-
+// Update an existing note by ID
+router.patch("/notes/:id", updateNote);
+// Deletes a note
+router.delete("/notes/:id", deleteNote);
 // Calls into the formatNote controller.
 router.post("/notes/:id/format", formatNote);
+// Maybe add exports?
 
 module.exports = router;
